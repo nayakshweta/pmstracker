dd1fb9d8976a3eb1752b9ba3157ae5bac9426867
shweta <nayakshweta07@gmail.com>
2019-06-14 19:00:32 +0530

    Adds method to get the pms earning for given pmrId, year and month

--- monthly_pms_earnings.py
+++ monthly_pms_earnings.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 import urllib
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def get_sebi_report_page():
     queryurl = Request('https://www.sebi.gov.in/sebiweb/other/OtherAction.do?doPmr=yes')
@@ -19,8 +20,21 @@ def get_pmrids(report):
 
     return pmrId_list
 
-def get_pms_earning_for_given_month_and_year():
-    pass
+def get_pms_earning_for_given_month_and_year(pmrId, year, month):
+    url_data = "pmrID" + pmrId + "&year" + str(year) + "&month" + str(month)
+    url_data = url_data.encode('ascii')
+
+    queryurl = Request('https://www.sebi.gov.in/sebiweb/other/OtherAction.do?doPmr=yes', url_data)
+    pms_earning_report = urlopen(queryurl).read()
+
+    pms_earning_soup = BeautifulSoup(pms_earning_report, 'html.parser')
+    
+    discretionary_services_div = pms_earning_soup.find_all('div', attrs = {'class' : 'org-table-1'})[1]
+    discretionary_services_td = discretionary_services_div.find_all('td')
+    portfolio_perf_in_percentage = list(discretionary_services_td[3].children)[0]
+    
+    return portfolio_perf_in_percentage
+
 
 def write_data_to_csv():
     pass
@@ -28,7 +42,24 @@ def write_data_to_csv():
 def main():
     report = get_sebi_report_page()
     pmrId_list = get_pmrids(report)
-    print(pmrId_list)
+    
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
+
+    year = 2013                 ## First year for which data is available on the SEBI webpage
+
+    for pmrId in pmrId_list:
+        while year <= currentYear:
+            month = 1
+            if year == currentYear:
+                while (month <= 12) or (month <= currentMonth):
+                    portfolio_perf_in_percentage = get_pms_earning_for_given_month_and_year(pmrId, year, month)
+                    month += 1
+            else:
+                while month <= 12:
+                    portfolio_perf_in_percentage = get_pms_earning_for_given_month_and_year(pmrId, year, month)
+                    month += 1
+            year += 1
 
 if __name__ == "__main__":
     main()
